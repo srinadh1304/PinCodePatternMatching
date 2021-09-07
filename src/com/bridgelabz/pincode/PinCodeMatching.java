3f305064e6bccac956bd22530c7d7a45e1e2aae2
srinadh1304 <srinadh1304@gmail.com>
2021-09-07 18:31:44 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/pincode/PinCodeMatching.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.pincode;
+
+
+public class PinCodeMatching {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to pin code matching program");
+	}
+}
