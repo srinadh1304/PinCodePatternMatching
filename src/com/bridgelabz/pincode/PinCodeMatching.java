04763aa0011b4e4b632272afa40059a837aac7e7
srinadh1304 <srinadh1304@gmail.com>
2021-09-07 18:36:48 +0530

    ADDED: Checking if pincode is 6 digits

--- src/com/bridgelabz/pincode/PinCodeMatching.java
+++ src/com/bridgelabz/pincode/PinCodeMatching.java
@@ -1,9 +1,24 @@
 package com.bridgelabz.pincode;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class PinCodeMatching {
+
+	public static boolean pincodePatternMatcher(String pincode) {
+		Pattern pattern = Pattern.compile("[0-9]{6}");
+		Matcher matcher = pattern.matcher(pincode);
+		return matcher.matches();
+	}
+
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to pin code matching program");
+		System.out.println("Welcome to Pin code matching program");
+		String pincode="400088";
+		if(pincodePatternMatcher(pincode))
+			System.out.println("It is a valid pincode");
+		else 
+			System.out.println("It is an invalid pincode");
+
 	}
 }
